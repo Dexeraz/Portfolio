16a27ebd4b8ec967aadc4bdd273a675521957e87
MBilik <dexeraz@gmail.com>
2023-03-10 10:38:08 +0100

    Navbar implemented

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
 import { BrowserRouter } from 'react-router-dom'
-import {About, Contact, Experience, Feedbacks, Hero, Navbar, Tech, Works, StarCanvas} from './components'
+import {About, Contact, Experience, Feedbacks, Hero, Navbar, Tech, Works, StarsCanvas} from './components'
 
 
 const App = () => {
   return (
     <BrowserRouter>
-      <div className='relative z-0 bg-primary'> 
+      <div className='relative z-0 bg-primary'>
         <div className='bg-hero-pattern bg-cover bg-no-repeat bg-center'>
           <Navbar />
           <Hero />
@@ -18,11 +18,11 @@ const App = () => {
         <Feedbacks />
         <div className='relative z-0'>
           <Contact />
-          <StarCanvas />
+          <StarsCanvas />
         </div>
       </div>
     </BrowserRouter>
-  )
+  );
 }
 
 export default App
