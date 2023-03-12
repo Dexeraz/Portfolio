7ffd3192a7afaf86053f1011a4f57f7432522682
MBilik <dexeraz@gmail.com>
2023-03-12 21:31:42 +0100

    Styling

--- src/components/canvas/Car.jsx
+++ src/components/canvas/Car.jsx
@@ -64,7 +64,7 @@ const CarsCanvas = () => {
       <Suspense fallback={<CanvasLoader />}>
         <OrbitControls
           autoRotate
-          autoRotateSpeed={0.3}
+          autoRotateSpeed={0.1}
           enableZoom={false}
           maxPolarAngle={Math.PI / 2}
           minPolarAngle={Math.PI / 2}
